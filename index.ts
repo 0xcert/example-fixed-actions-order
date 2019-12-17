671ec664be88e5c52e795cd831a033a069001572
Tadej Vengust <vengust.tadej@gmail.com>
2019-12-17 10:18:10 +0100

    Add basic style skeleton.

---
+++ index.ts
@@ -0,0 +1,30 @@
+const divConsole = document.getElementById("console");
+
+function printError(message: any) {
+  if (typeof message !== "string") {
+    message = JSON.stringify(message, null, 2);
+  }
+  const div = document.createElement("div");
+  div.innerText = "Error: " + message;
+  div.className = "error";
+  divConsole.prepend(div);
+}
+
+function printWarning(message: any) {
+  if (typeof message !== "string") {
+    message = JSON.stringify(message, null, 2);
+  }
+  const div = document.createElement("div");
+  div.innerText = "Warning: " + message;
+  div.className = "warning";
+  divConsole.prepend(div);
+}
+
+function printMessage(message: any) {
+  if (typeof message !== "string") {
+    message = JSON.stringify(message, null, 2);
+  }
+  const div = document.createElement("div");
+  div.innerText = message;
+  divConsole.prepend(div);
+}
